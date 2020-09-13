134fd15d97a28ea599ab256e274a08087fec82f7
Eduardo Marques <edu.temarques@gmail.com>
2020-09-13 00:22:53 -0300

    Initial commit

---
+++ src/test/java/br/com/person/crud/api/PersonCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.person.crud.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
