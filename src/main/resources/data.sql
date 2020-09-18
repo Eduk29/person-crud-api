8d3000bdb2f97fac7c377016a9d96a137c8c58a9
Eduardo Marques <edu.temarques@gmail.com>
2020-09-18 02:08:16 -0300

    chore: Add database configurations

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO persons_crud_db.crud_person (id_person, name_person, actor_name_person, height_person, mass_person, birthday_person, gender_person, homeworld_person, species_person, created_person, edited_person)
+values (NEXT VALUE FOR persons_crud_db.seq_crud_person, 'Luke Skywalker', 'Mark Hamill', 172, 77, '1978-08-13', 'Male', 'Tatooine', 'Human','2014-12-09', '2014-12-20');
+
+
+INSERT INTO persons_crud_db.crud_person (id_person, name_person, actor_name_person, height_person, mass_person, birthday_person, gender_person, homeworld_person, species_person, created_person, edited_person)
+values (NEXT VALUE FOR persons_crud_db.seq_crud_person, 'C-3PO', 'Anthony Daniels', 167, 75, '1952-02-21', 'Not Defined', 'Tatooine', 'Droid','2014-12-09', '2014-12-20');
+
+
+INSERT INTO persons_crud_db.crud_person (id_person, name_person, actor_name_person, height_person, mass_person, birthday_person, gender_person, homeworld_person, species_person, created_person, edited_person)
+values (NEXT VALUE FOR persons_crud_db.seq_crud_person, 'R2-D2', 'Kenny Baker', 96, 32, '1952-07-12', 'Not Defined', 'Naboo', 'Droid','2014-12-09', '2014-12-20');
+
+
+INSERT INTO persons_crud_db.crud_person (id_person, name_person, actor_name_person, height_person, mass_person, birthday_person, gender_person, homeworld_person, species_person, created_person, edited_person)
+values (NEXT VALUE FOR persons_crud_db.seq_crud_person, 'Chewbacca', 'Peter MAyhew', 228, 112, '1965-06-02', 'Male', 'Kashyyyk', 'Wookie','2014-12-09', '2014-12-20');
